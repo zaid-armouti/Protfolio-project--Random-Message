d0bda863e8dd747ef7cb17e06f55dc2ea68bd296
Zaid Armouti <zaid.m.armouti@gmail.comgit config --global user.email zaid.m.armouti@gmail.com>
2023-06-01 01:24:19 +0300

    First commit: README and basic script add.

---
+++ README.md
@@ -0,0 +1,15 @@
+Project: Random Motivation Quots.
+=================================
+
+This program will provide different motivational Quots said by famous figures.
+-------------------------------------------------------------------------------
+
+According to the user input the type of the motivation feedback will change.
+If someone got the wrong motivation their energy might shift to the wrong direction, leads to waisting their energy.
+
+Here are the types of motivational quots that will be provided:
+1. Physical Motivation.
+2. Studying Motivation.
+3. Emotional Motivation.
+
+Each type of these will have at least 4 different quots that will change each time the program starts.
---
+++ script.js
@@ -0,0 +1,38 @@
+// Pysical motivation.
+const pysical1 = "Strength does not come from physical capacity. It comes from indomitable will. -Mahatma Gandhi";
+const pysical2 = "It's not about how hard you can hit, its about hard you can get hit and keeping moving forward. -Rocky Balbowa";
+const pysical3 = "If you want something you’ve never had, you must be willing to do something you’ve never done. -Thomas Jefferson";
+const pysical4 = "GET YOUR ASS OFF THE CHAIR AND GET SOME WORK DONE. -Zaid Armouti"
+// add more messages above.
+const pysical = [pysical1, pysical2, pysical3, pysical4];
+let num1 = Math.floor(Math.random()*4);
+console.log(pysical[num1]);
+
+
+// students Motivation.
+const students1 = "Choose the path that will stop you from working a day in your life.";
+const students2 = "It does not matter people think as long as you love your major.";
+const students3 = "If a major worked for someone it does not have to work for you too.";
+const students4 = "Do not follow your elders foot steps if you do not love it.";
+// add more messages above.
+const students = [students1, students2, students3, students4];
+let num2 = Math.floor(Math.random()*4);
+console.log(students[num2]);
+
+
+// emotional motivation.
+const emotional1 = "With strong emotions we form strong memories carved in our brains.";
+const emotional2 = "The way you choose to deal with your emotions determin your next step.";
+const emotional3 = "Dont let your brain turn a bad few moments to a bad life.";
+const emotional4 = "Emotional mastery is something important to learn.";
+// add more messages above.
+const emotional = [emotional1, emotional2, emotional3, emotional4];
+let num3 = Math.floor(Math.random()*4);
+console.log(emotional[num3]);
+
+
+
+// console.log(num1)
+// console.log(num2)
+// console.log(num3)
+// console.log(num4)
